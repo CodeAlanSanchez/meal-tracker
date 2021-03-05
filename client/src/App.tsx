aad70e18b185580c67fbec272fb488c99a8fc2a3
Alan Sanchez <realalansan@gmail.com>
2021-03-04 20:55:38 -0800

    Enclose home components in Home component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import About from './components/Main/About/About';
-import Contact from './components/Main/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Hero from './components/Main/Hero/Hero';
 import Tracker from './components/Tracker/Tracker';
 import './styles.css';
+import Home from './components/Home/Home';
 
 const App = () => (
   <>
@@ -14,9 +12,7 @@ const App = () => (
     <main>
       <Switch>
         <Route exact path={['/', '/home']}>
-          <Hero />
-          <About />
-          <Contact />
+          <Home />
         </Route>
         <Route path="/tracker">
           <Tracker />
