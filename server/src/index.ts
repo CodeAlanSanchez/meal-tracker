b56cdb413b26b78a8bd25688ac87d81d2c178c5c
Alan Sanchez <realalansan@gmail.com>
2021-03-08 23:58:48 -0800

    Fix routes not using cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,12 +11,12 @@ dotenv.config();
 
 const app = express();
 
-app.use('/meals', mealRoutes);
-app.use('/users', userRoutes);
-
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+app.use('/meals', mealRoutes);
+app.use('/users', userRoutes);
 
 declare let process : {
   env: {
